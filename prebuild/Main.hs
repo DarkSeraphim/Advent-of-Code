7f375bf34e75c77efd7952c56443329e230fd476
Mark Hendriks <mark.hendriks@hybrit.org>
2021-11-30 11:25:01 +0100

    Fix app/Main.hs by using a template file (to avoid file locks)

--- prebuild/Main.hs
+++ prebuild/Main.hs
@@ -1,7 +1,9 @@
 import Data.List (elemIndex, intercalate)
-import Helpers.Input (maybeIO)
+import Helpers.Input (maybeIO, readInteger)
 import System.Directory (listDirectory)
 import Debug.Trace
+import System.IO (openFile, IOMode (ReadWriteMode), hGetContents, hPutStr, hClose)
+import Text.Printf (printf)
 
 isDay ('D':'a':'y':number) = [number]
 isDay _ = []
@@ -22,15 +24,19 @@ findDays = concatMap isDay <$> listDirectory "src/"
 findParts :: [Char] -> IO [(String, String, [Char])]
 findParts day = concatMap (getPart day . removeExt) <$> listDirectory ("src/Day" ++ day)
 
-buildImport day = "import Day" ++ day
-buildSolve (day, part, mod) = "solve " ++ "\"" ++ day ++ "\" " ++ "\"" ++ part ++ "\" = " ++ mod ++ ".solve"
+buildImport mod = "import " ++ mod
+buildSolve :: (String, String, String) -> String
+buildSolve (day, part, mod) = printf "solveDay \"%s\" \"%s\" = %s.solve" d part mod
+  where d = show (readInteger day)
+
+getMod (_, _, mod) = mod
 
 main = do
-  contents <- lines <$> readFile "app/Main.hs"
+  contents <- lines <$> readFile "app/Main.hs.template"
   start <- maybeIO $ elemIndex "-- AUTOGEN-START" contents
   end <- maybeIO $ elemIndex "-- AUTOGEN-END" contents
   days <- findDays
   parts <- concat <$> mapM findParts days
-  let imports = map buildImport days
+  let imports = map (buildImport . getMod) parts
   let solves = map buildSolve parts
-  putStrLn $ intercalate "\n" (take (start + 1) contents ++ imports ++ solves ++ drop end contents)
+  writeFile "app/Main.hs" $ intercalate "\n" (take (start + 1) contents ++ imports ++ solves ++ drop end contents)
