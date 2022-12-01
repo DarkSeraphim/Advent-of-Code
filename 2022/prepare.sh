5d0c1a9e19211a18ddd06a2b7d35128dd966aaf6
Mark Hendriks <mark.hendriks@hybrit.org>
2022-12-01 13:41:38 +0100

    Remove whitespace from the file, as by default Haskell doesn't indent module content

--- 2022/prepare.sh
+++ 2022/prepare.sh
@@ -4,17 +4,18 @@ mkdir -p src/Day$day
 
 cat <<eof > src/Day$day/Part1.hs
 module Day$day.Part1 (solve) where
-    import Text.Printf (printf)
-    solve = do
-      _ <- getContents
-      printf "Day $1 part 1 has not been implemented yet" 
+import Text.Printf (printf)
+
+solve = do
+  _ <- getContents
+  printf "Day $1 part 1 has not been implemented yet" 
 eof
 
 cat <<eof > src/Day$day/Part2.hs
 module Day$day.Part2 (solve) where
-    import Text.Printf (printf)
-    solve = do
-      _ <- getContents
-      printf "Day $1 part 2 has not been implemented yet" 
+import Text.Printf (printf)
+solve = do
+  _ <- getContents
+  printf "Day $1 part 2 has not been implemented yet" 
 eof
 
