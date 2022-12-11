a0af1fb1531548e7aba773104e2a69fb4ab73475
Mark Hendriks <mark.hendriks@hybrit.org>
2022-12-11 01:32:28 +0100

    Whoops forgot a rebuildPath function

--- 2022/src/Helpers/Graph.hs
+++ 2022/src/Helpers/Graph.hs
@@ -1,18 +1,26 @@
-module Helpers.Graph (bfs, dijkstra, PathResult) where
-import Data.Map (Map, empty, findWithDefault)
+module Helpers.Graph (bfs, dijkstra, dijkstra', PathResult) where
+import Data.Map (Map, empty, findWithDefault, (!))
 import Data.Set (Set, findMin, union, deleteFindMin, fromList, notMember, member, singleton)
 import qualified Data.Set as S
 import qualified Data.Map as M
 
 data PathResult a = PathResult {dist :: Int, parent :: a}
 
-dijkstra :: Ord a => Map (a, a) Int -> Map a [a] -> a -> [a]
+rebuildPath :: Ord a => a -> Map a (PathResult a) -> [a]
+rebuildPath cur res
+  | cur == p  = [cur]
+  | otherwise = cur : rebuildPath p res
+  where res' = res ! cur
+        p = parent res'
+
+dijkstra :: Ord a => Map (a, a) Int -> Map a [a] -> a -> a -> [a]
 dijkstra weights = dijkstra' (\k -> findWithDefault maxBound k weights)
 
-dijkstra' :: Ord a => ((a, a) -> Int) -> Map a [a] -> a -> [a]
-dijkstra' wfunc edges start = []
+dijkstra' :: Ord a => ((a, a) -> Int) -> Map a [a] -> a -> a -> [a]
+dijkstra' wfunc edges start end = rebuildPath end res
   where res = dijkstra'' wfunc edges S.empty (singleton (0, start, start))
 
+-- | This implementation currently doesn't stop at the end node, but computes the full graph
 dijkstra'' :: Ord a => ((a, a) -> Int) -> Map a [a] -> Set a -> Set (Int, a, a) -> Map a (PathResult a)
 dijkstra'' wfunc edges visited queue
   | null queue = empty
@@ -23,5 +31,5 @@ dijkstra'' wfunc edges visited queue
         newEdges = filter (`notMember` visited') (findWithDefault [] cur edges)
         queue'' = queue' `union` fromList (map (\v -> (w + wfunc (cur, v), v, cur)) newEdges)
 
-bfs :: Ord a => Map a [a] -> a -> [a]
+bfs :: Ord a => Map a [a] -> a -> a -> [a]
 bfs = dijkstra' (const 1)
