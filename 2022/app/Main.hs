333bc87315cbb3284ecd0bfbd428d08c7f2864c1
Mark Hendriks <mark.hendriks@hybrit.org>
2022-12-14 20:02:52 +0100

    Submit day 14

--- 2022/app/Main.hs
+++ 2022/app/Main.hs
@@ -33,6 +33,8 @@ import Day04.Part1
 import Day04.Part2
 import Day12.Part1
 import Day12.Part2
+import Day14.Part1
+import Day14.Part2
 solveDay "13" "1" = Day13.Part1.solve
 solveDay "13" "2" = Day13.Part2.solve
 solveDay "2" "1" = Day02.Part1.solve
@@ -59,6 +61,8 @@ solveDay "4" "1" = Day04.Part1.solve
 solveDay "4" "2" = Day04.Part2.solve
 solveDay "12" "1" = Day12.Part1.solve
 solveDay "12" "2" = Day12.Part2.solve
+solveDay "14" "1" = Day14.Part1.solve
+solveDay "14" "2" = Day14.Part2.solve
 -- AUTOGEN-END
 
 -- solve :: String -> String -> IO ()
---
+++ 2022/src/Day14/Part2.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE TupleSections #-}
+module Day14.Part2 (solve) where
+import Text.Printf (printf)
+import Lib (Point (x, y), newPoint)
+import Helpers.Parsec (Parser, number, parseInput)
+import Text.Parsec (char, sepBy1, string, endOfLine, endBy1)
+import Data.Map (fromList, member, keys, findWithDefault, Map, notMember, insert)
+import qualified Data.Map as M
+import Data.List (intercalate)
+
+parsePoint :: Parser Point
+parsePoint = newPoint <$> (number <* char ',') <*> number
+
+parseLine :: Parser [Point]
+parseLine = parsePoint `sepBy1` string " -> "
+
+parse :: Parser [[Point]]
+parse = parseLine `endBy1` endOfLine
+
+getLines :: (Point, Point) -> [Point]
+getLines (from, to)
+  | from == to = [from]
+  | otherwise  = from : getLines (from', to)
+  where from' = from + signum (to - from)
+
+down = newPoint 0 1
+left = newPoint (-1) 1
+right = newPoint 1 1
+
+simulateSand :: Point -> Map Point a -> Int -> Point
+simulateSand start grid lowest
+  | y down' == lowest = start
+  | down' `notMember` grid = simulateSand down' grid lowest
+  | left' `notMember` grid = simulateSand left' grid lowest
+  | right' `notMember` grid = simulateSand right' grid lowest
+  | otherwise = start
+  where down' = start + down
+        left' = start + left
+        right' = start + right
+
+run :: Point -> Map Point Char -> Int -> Map Point Char
+run start grid lowest =
+  if next == start then putSand next else run start (putSand next) lowest
+  where next = simulateSand start grid lowest
+        putSand pos = insert pos 'o' grid
+
+solve = do
+  lines <- parseInput parse
+  let points = fromList $ map (,'#') $ concatMap (\lines' -> concat $ zipWith (curry getLines) lines' (tail lines')) lines
+  let start = newPoint 500 0
+
+  let minX = minimum $ map x $ keys points
+  let maxX = maximum $ map x $ keys points
+  let minY = 0 --minimum $ map y $ keys points
+  let maxY = maximum $ map y $ keys points
+
+  let c = map (\y -> map (\x -> findWithDefault '.' (newPoint x y) points) [minX..maxX]) [minY..maxY]
+
+  printf "Grid:\n%s\n" (intercalate "\n" c)
+
+  let result = run start points (maxY + 2)
+  let c = map (\y -> map (\x -> findWithDefault '.' (newPoint x y) result) [minX..maxX]) [minY..maxY]
+
+  printf "Grid:\n%s\n" (intercalate "\n" c)
+  printf "%d units of sand rest on the cave\n" (length $ filter (=='o') $ M.elems result)
