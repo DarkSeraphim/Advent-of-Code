83fb615c1bcb36122cedbd2abcdb9dc78a45fbd8
Mark Hendriks <mark.hendriks@openvalue.nl>
2023-12-01 07:46:00 +0100

    2023-1

--- 2023/src/Day01/Part1.hs
+++ 2023/src/Day01/Part1.hs
@@ -1,6 +1,14 @@
 module Day01.Part1 (solve) where
 import Text.Printf (printf)
+import Data.Char (isDigit)
 
+combineNumber :: [a] -> [a]
+combineNumber l = [head l, last l]
+
+solve :: IO ()
 solve = do
-  _ <- getContents
-  printf "Day 1 part 1 has not been implemented yet" 
+  lines' <- lines <$> getContents
+  let lines'' = map (filter isDigit) lines'
+  let numbers = map ((read :: String -> Int) . combineNumber) lines''
+
+  printf "Calibration value is %d\n" (sum numbers)
--- 2023/src/Day01/Part2.hs
+++ 2023/src/Day01/Part2.hs
@@ -1,5 +1,31 @@
 module Day01.Part2 (solve) where
 import Text.Printf (printf)
+import Data.Char (isDigit)
+
+parse :: String -> String
+parse [] = []
+parse ('o':'n':'e':x) = '1' : parse ('e':x)
+parse ('t':'w':'o':x) = '2' : parse ('o':x)
+parse ('t':'h':'r':'e':'e':x) = '3' : parse ('e':x)
+parse ('f':'o':'u':'r':x) = '4' : parse x
+parse ('f':'i':'v':'e':x) = '5' : parse ('e':x)
+parse ('s':'i':'x':x) = '6' : parse x
+parse ('s':'e':'v':'e':'n':x) = '7' : parse ('n':x)
+parse ('e':'i':'g':'h':'t':x) = '8' : parse ('t':x)
+parse ('n':'i':'n':'e':x) = '9' : parse ('e':x)
+--parse ('z':'e':'r':'o':x) = '0' : parse x
+parse (d:x)
+  | isDigit d = d : parse x
+  | otherwise  = parse x
+
+combineNumber :: [a] -> [a]
+combineNumber l = [head l, last l]
+
+solve :: IO ()
 solve = do
-  _ <- getContents
-  printf "Day 1 part 2 has not been implemented yet" 
+  lines' <- lines <$> getContents
+  let lines'' = map parse lines'
+  let numbers = map ((read :: String -> Int) . combineNumber) lines''
+
+  printf "Calibration value is %d\n" (sum numbers)
+
