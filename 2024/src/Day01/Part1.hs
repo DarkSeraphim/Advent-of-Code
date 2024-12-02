d221e5fed943669a3aa4187c27304d468dea387f
Mark Hendriks <mark.hendriks@openvalue.nl>
2024-12-02 23:09:34 +0100

    Push day 1

--- 2024/src/Day01/Part1.hs
+++ 2024/src/Day01/Part1.hs
@@ -1,7 +1,19 @@
 module Day01.Part1 (solve) where
 import Text.Printf (printf)
+import Helpers.Parsec (parseInput, Parser, number, spaces)
+import Text.Parsec (sepEndBy1, endOfLine)
+import Data.List (transpose)
+import GHC.OldList (sort)
+
+numbers :: Parser [Int]
+numbers = sequence [number <* spaces, number]
+
+diff :: [Int] -> Int
+diff [a, b] = abs (a - b)
+diff _ = error "Invalid input"
 
 solve :: IO ()
 solve = do
-  s <- getContents
-  printf "Day 1 part 1 has not been implemented yet %s" s 
+  rows <- parseInput (numbers `sepEndBy1` endOfLine)
+  let res = sum $ map diff (transpose $ map sort (transpose rows))
+  printf "Sum of differences is %d\n" res 
--- 2024/src/Day01/Part2.hs
+++ 2024/src/Day01/Part2.hs
@@ -1,7 +1,24 @@
 module Day01.Part2 (solve) where
 import Text.Printf (printf)
+import Helpers.Parsec ( Parser, parseInput, number, spaces )
+import Text.Parsec (sepEndBy1, endOfLine)
+import Data.Map (Map, findWithDefault)
+import Helpers.List (frequency)
+import Data.List (transpose)
+
+numbers :: Parser [Int]
+numbers = sequence [number <* spaces, number]
+
+processInput :: [[Int]] -> ([Int], Map Int Int)
+processInput [left, right] = (left, frequency right)
+processInput _ = error "Invalid input"
+
+freqMult :: Map Int Int -> Int -> Int
+freqMult freq x = x * findWithDefault 0 x freq
 
 solve :: IO ()
 solve = do
-  _ <- getContents
-  printf "Day 1 part 2 has not been implemented yet" 
+  rows <- parseInput (numbers `sepEndBy1` endOfLine)
+  let (left, freq) = processInput (transpose rows)
+  let res = sum $ map (freqMult freq) left
+  printf "Frequency product sum is %d\n" res
