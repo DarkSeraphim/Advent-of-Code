ec8953fc38c7cb2b504672bf23016f402f03f58e
Mark Hendriks <mark.hendriks@openvalue.nl>
2025-12-02 12:19:47 +0100

    Day 2

--- 2025/src/Day02/Part1.hs
+++ 2025/src/Day02/Part1.hs
@@ -1,7 +1,25 @@
 module Day02.Part1 (solve) where
 import Text.Printf (printf)
+import Helpers.Parsec (Parser, number, parseInput)
+import Helpers.Range (Range, newRange, getMin, getMax)
+import Text.Parsec (char, sepBy1)
+
+rangeP :: Parser (Range Int)
+rangeP = newRange <$> number <*> (char '-' *> number)
+
+rangesP :: Parser [Range Int]
+rangesP = rangeP `sepBy1` char ','
+
+isValid :: Int -> Bool
+isValid num = take half s /= drop half s
+  where s = show num
+        half = length s `div` 2
+
+getInvalid :: Range Int -> [Int]
+getInvalid r = filter (not . isValid) [getMin r .. getMax r]
 
 solve :: IO ()
 solve = do
-  _ <- getContents
-  printf "Day 2 part 1 has not been implemented yet" 
+  ranges <- parseInput rangesP
+  let res = sum $ concatMap getInvalid ranges
+  printf "The sum of invalid numbers is %d\n" res 
--- 2025/src/Day02/Part2.hs
+++ 2025/src/Day02/Part2.hs
@@ -1,7 +1,33 @@
 module Day02.Part2 (solve) where
 import Text.Printf (printf)
+import Helpers.Parsec (Parser, number, parseInput)
+import Helpers.Range (Range, newRange, getMin, getMax)
+import Text.Parsec (char, sepBy1)
+import Helpers.List (chunksOf)
+
+rangeP :: Parser (Range Int)
+rangeP = newRange <$> number <*> (char '-' *> number)
+
+rangesP :: Parser [Range Int]
+rangesP = rangeP `sepBy1` char ','
+
+isSame2 :: [String] -> Bool
+isSame2 [] = False
+isSame2 [_] = False
+isSame2 [x, y] = x == y
+isSame2 (x:y:rest) = x == y && isSame2 (y:rest)
+
+isValid :: Int -> Bool
+isValid num = not $ any (\n -> isSame2 $ chunksOf n s) $ filter (\n -> (len `rem` n) == 0) [1 .. len]
+  where s = show num
+        len = length s
+        
+
+getInvalid :: Range Int -> [Int]
+getInvalid r = filter (not . isValid) [getMin r .. getMax r]
 
 solve :: IO ()
 solve = do
-  _ <- getContents
-  printf "Day 2 part 2 has not been implemented yet" 
+  ranges <- parseInput rangesP
+  let res = sum $ concatMap getInvalid ranges
+  printf "The sum of invalid numbers is %d\n" res 
