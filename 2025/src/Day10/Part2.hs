cebf7608633de9382c8169136dea8c715cd267a4
Mark Hendriks <mark.hendriks@openvalue.nl>
2026-01-05 00:16:22 +0100

    Attempt a new impl of matrix REF

--- 2025/src/Day10/Part2.hs
+++ 2025/src/Day10/Part2.hs
@@ -1,23 +1,18 @@
-{-# LANGUAGE TupleSections #-}
-module Day10.Part2 (solve) where
+module Day10.Part2 (solve, bruteForce, combinationPresses) where
 import Text.Printf (printf)
 import Helpers.Parsec
 import Text.Parsec hiding (space)
-import Helpers.Graph (dijkstra''', computeDistance)
-import Data.Bits (Bits(xor))
-import Data.Set (empty, singleton)
-import Data.Maybe (fromJust)
-import Data.IntMap.Strict (IntMap, fromList, unionWith, filterWithKey, (!), unionsWith, insertWith, keys)
-import Helpers.List (cycleN, head', tail')
-import Data.List (sortOn, sort, nub, maximumBy, transpose)
-import Control.Applicative (asum)
+import Helpers.List (head')
+import Data.List (sortOn, transpose)
 import Debug.Trace (trace)
-import qualified Data.IntMap as M
-import Data.List (partition)
 import Helpers.Matrix (augmentColumns, rowEchelonForm, unaugmentColumn, showMatrix)
-import Data.Fixed (mod')
-import qualified Data.Set as S
-
+import Data.IntMap.Strict (IntMap, empty)
+import qualified Data.Sequence as S
+import Data.Foldable (toList)
+import Data.IntMap.Strict ( fromList, insert, member, union )
+import qualified Data.IntMap as M
+import Data.Maybe (fromJust)
+import Data.List (find)
 
 data Input = Input {output :: [Bool], toggles :: [[Int]], joltageRequirements :: [Int]}
 
@@ -42,82 +37,15 @@ inputP =  Input <$> (outputP <* space) <*> togglesP <*> joltageReqP
 inputsP :: Parser [Input]
 inputsP = inputP `endBy1` endOfLine
 
-encodeToggle :: [Int] -> IntMap Int
-encodeToggle ts = fromList $ map (,1) ts
-
-encodeState :: [Int] -> IntMap Int
-encodeState js = fromList $ zip [0..] js
-
-isPossible :: IntMap Int -> IntMap Int -> Bool
-isPossible end state = null $ filterWithKey (\k v -> (end ! k) < v) state
-
-sortButtonOnImpact :: [[Int]] -> IntMap Int -> [[Int]]
-sortButtonOnImpact buttons joltages = sortOn f buttons
-  where outputCount = unionsWith (+) $ map encodeToggle buttons
-        f a = (minimum (map (outputCount !) a), sort $ map (\b -> -(joltages ! b)) a)
-
-canPress :: IntMap Int -> [[Int]] -> [[Int]]
-canPress state = filter f
-  where f = all ((>0) . (state !))
-
-canComplete :: IntMap Int -> [[Int]] -> Bool
-canComplete delta buttons = all (`elem` allButtons) $ keys (M.filter (/=0) delta)
-  where allButtons = nub $ sort $ concat buttons
-
-applyPresses :: IntMap Int -> [Int] -> Int -> IntMap Int
-applyPresses state buttons n = foldl f state buttons
-  where f state' b' = insertWith (+) b' n state'
-
-minRequiredNotPossible :: IntMap Int -> [[Int]] -> Bool
-minRequiredNotPossible delta remainingButtons = or (M.mapWithKey f delta)
-  -- f checks if there is a key, for which the value required is more than the available presses
-  -- we can probably prune more, because this doesn't consider the side effects between buttons
-  where f k v = v > sum (map (maxPresses delta) $ filter (k `elem`) remainingButtons)
-
-maxPresses :: IntMap Int -> [Int] -> Int
-maxPresses delta button = minimum $ map (delta !) button
-
-combinationPresses :: Int -> Int -> [[Int]]
-combinationPresses       0 buttons = [replicate buttons 0]
-combinationPresses toSplit       1 = [[toSplit]]
-combinationPresses       _       0 = error "Can this happen?"
-combinationPresses toSplit buttons = concatMap rec cur
-  where cur = reverse [0..toSplit]
-        rec n = map (n :) $ combinationPresses (toSplit - n) (buttons - 1)
-
-dfs2 :: [[Int]] -> IntMap Int -> IntMap Int -> Maybe Int
-dfs2 buttons goal state
-  | goal == state = Just 0
-  | null delta = Nothing
-  | any (<0) delta = Nothing
-  | null applicableButtons = Nothing
-  | otherwise = (joltageValue + ) <$> asum (map (dfs2 restButtons goal . applyCombination applicableButtons state) combinations)
-  where delta = M.filter (>0) $ unionWith (-) goal state
-        outputCount = unionsWith (+) $ map encodeToggle buttons
-        (joltageIdx, joltageValue) = maximum $ sortOn (\(k, v) -> (outputCount ! k, -v)) (M.toList delta)
-        (applicableButtons, restButtons) =  partition (joltageIdx `elem`) buttons
-        combinations = combinationPresses joltageValue (length applicableButtons)
-        applyCombination :: [[Int]] -> IntMap Int -> [Int] -> IntMap Int
-        applyCombination buttons' state' comb = state''
-          where state'' =  foldl (\s (n, b) -> applyPresses s b n) state' $ filter ((/= 0) . fst) (zip comb buttons')
-
-
-dfs :: [[Int]] -> IntMap Int -> IntMap Int -> Maybe Int
-dfs buttons goal state
--- we're done
-  | goal == state = Just 0
--- we can achieve the sum of the buttons
-  | minRequiredNotPossible diff buttonsLeft = Nothing
--- everything we need to be able to press is remaining
-  | not $ canComplete diff buttonsLeft = Nothing
--- nothing left to press, but not done
-  | null buttonsLeft = Nothing
-  | otherwise = asum $ map rec (reverse [0..maxPresses'])
-  where diff = unionWith (-) goal state
-        buttonsLeft = canPress diff buttons
-        pressing = head' buttonsLeft
-        maxPresses' = maxPresses diff pressing
-        rec n = (n +) <$> dfs (drop 1 buttonsLeft) goal (applyPresses state pressing n)
+-- TODO: optimise this
+-- Maybe order negative/positive first, so we can abort early (now we're forced to generate many impossible solutions)
+combinationPresses :: Int -> S.Seq Int -> Int -> [[Int]]
+combinationPresses 0 buttons _ = [replicate (S.length buttons) 0]
+combinationPresses toSplit (coef S.:<| S.Empty) _ = [[toSplit `div` coef]]
+combinationPresses       _              S.Empty _ = error "Can this happen?"
+combinationPresses toSplit    (coef S.:<| rest) maxPress = concatMap rec cur
+  where cur = reverse [0..maxPress]
+        rec n = map (n :) $ combinationPresses (toSplit - (n * coef)) rest maxPress
 
 buildButtonMatrix :: Int -> [[Int]] -> [[Int]]
 buildButtonMatrix joltageCount buttons = transpose buttonCoefficients
@@ -128,53 +56,63 @@ buildLPMatrix input = augmentColumns buttonsMatrix joltages
   where joltages = joltageRequirements input
         buttonsMatrix = buildButtonMatrix (length joltages) (toggles input)
 
-bruteForce :: Int -> [Int] -> [[Int]]
+-- TODO: check if we have to optimise?
+bruteForce :: Int -> Int -> S.Seq (Int, Int) -> [IntMap Int]
 -- If it's too big, this is a likely issue
-bruteForce sol coef = takeWhile ((minSum ==) . sum)  sols
-  where presses = combinationPresses sol (length coef)
-        isSol p = sol == sum (zipWith (*) coef p)
-        sols = sortOn sum (filter isSol presses)
-        minSum = sum $ head' sols
+bruteForce bruteForceMultiplier sol coef = map (fromList . toList) sols
+  where coef' = snd <$> coef
+        presses = map S.fromList $ combinationPresses sol coef' ((bruteForceMultiplier) * maximum (fmap (abs . (sol `div`)) coef'))
+        isSol :: S.Seq Int -> Bool
+        isSol p = all (>= 0) p && (sol == sum (S.zipWith (*) coef' p))
+        sols = map withIndex $ sortOn sum (filter isSol presses)
+        withIndex = S.zipWith (\(i, _) v -> (i, v)) coef
 
 
-solveRow :: [Int] -> [[Int]] -> Int -> [[Int]]
-solveRow coefs partials const' = concatMap nFree partials
-  where nonZero = dropWhile (==0) coefs
-        freeCount partial = length nonZero - length partial
-        partialSol partial = const' - sum (zipWith (*) (reverse partial) (reverse coefs))
-        nFree partial = if 1 == freeCount partial then [oneFree partial] else moreFree partial
-        oneFree partial = trace (printf "one free for partial %s\n" (show partial)) $ partialSol partial `div` head' nonZero : partial
-        moreFree partial = trace (printf "more free for partial %s\n" (show partial)) map ( ++ partial) $ bruteForce (partialSol partial) (take (freeCount partial) nonZero)
+solveRow :: Int -> [Int] -> [IntMap Int] -> Int -> [IntMap Int]
+solveRow bruteForceMultiplier coefs partials const' = concatMap nFree partials
+  where coefs' = S.fromList coefs
+        nonZero :: S.Seq Int -> S.Seq (Int, Int)
+        nonZero s = S.filter ((/= 0) . snd) $ S.mapWithIndex (,) s
+        nonZeroCoefs = nonZero coefs'
+        freeVariables :: IntMap Int -> S.Seq (Int, Int) -- idx, coeficient
+        freeVariables partial = S.filter (not . (`member` partial) . fst) nonZeroCoefs
+        freeCount :: IntMap Int -> Int
+        freeCount = S.length . freeVariables
+        partialSol :: IntMap Int -> Int
+        partialSol partial = const' - sum (map (\(i, v) -> v * (coefs' `S.index` i)) $ M.toList partial)
+        nFree :: IntMap Int -> [IntMap Int]
+        nFree partial = if 1 == freeCount partial then oneFree partial else moreFree partial
+        oneFree :: IntMap Int -> [IntMap Int]
+        oneFree partial
+          | sol >= 0 && sol * freeCoef == sol' = {-trace (printf "one for %s solving %d. sol' was %d and freeCoef was %d\n" (show partial) const' sol' freeCoef) $-} [insert freeIdx (sol' `div` freeCoef) partial]
+          | sol >= 0 = {-trace (printf "solution was not fitting integer space: sol=%d sol'=%d freeCoef=%dm const'=%d" sol sol' freeCoef const') -}[]
+          | otherwise = {-trace (printf "sol was negative: sol=%d, sol'=%d, freeCoef=%d, const'=%d, partial=%s" sol sol' freeCoef const' (show partial))-} []
+          where sol' = partialSol partial
+                sol = sol' `div` freeCoef
+                (freeIdx, freeCoef) = head' $ toList $ freeVariables partial
+        moreFree partial = {-trace (printf "more for %s\nnamely:\n%s\n\n" (show partial) (show newPartials)) $-} map (union partial) newPartials
+          where newPartials = bruteForce bruteForceMultiplier sol' (freeVariables partial)
+                sol' = partialSol partial
 
-solveMatrixBackwards :: [[Int]] -> [Int] -> [[Int]]
-solveMatrixBackwards [] [] = [[]]
-solveMatrixBackwards (coef:coefs) (const':consts) = trace (printf "Solution: %s\n" (show sol)) sol 
-  where partial = solveMatrixBackwards coefs consts
-        sol = solveRow coef partial const'
-
-solveMatrixBackwards _ _ = error "Weird matrix"
-
-dedupeMatrix :: [[Int]] -> [[Int]]
-dedupeMatrix m = dedupe m S.empty
-  where dedupe [] _ = []
-        dedupe (r:rest) seen
-          | r `S.member` seen = dedupe rest seen
-          | otherwise = r : dedupe rest (S.insert r seen)
+solveMatrixBackwards :: Int -> [[Int]] -> [Int] -> [IntMap Int]
+solveMatrixBackwards _ [] [] = [empty]
+solveMatrixBackwards n (coef:coefs) (const':consts) = sol
+  where partial = solveMatrixBackwards n coefs consts
+        sol = solveRow n coef partial const'
+solveMatrixBackwards _ _ _ = error "Weird matrix"
 
 solveForInput :: (Int, Input) -> IO Int
 solveForInput (idx, input) = do
     let matrix = buildLPMatrix input
-        refMatrix = rowEchelonForm (dedupeMatrix matrix)
+        refMatrix = rowEchelonForm matrix
         (coefs, consts) = unaugmentColumn refMatrix
-        solution = solveMatrixBackwards coefs consts
+        solution = fromJust $ find (not . null) $ map (\n -> solveMatrixBackwards n coefs consts) [1..10]
     printf "Matrix:\n%s\n\n" (showMatrix matrix)
-    printf "Deduped:\n%s\n\n" (showMatrix $ dedupeMatrix matrix)
+    printf "Ref:\n%s\n\n" (showMatrix refMatrix)
     let res = minimum $ map sum solution
     printf "%d has result %d\n" idx res
+    printf "actual presses were %s\n" (show (head' $ sortOn (sum) solution))
     return res
-    where js = encodeState $ joltageRequirements input
-          ts = sortButtonOnImpact (toggles input) js
-          start = fromList $ map (,0) $ keys js
 
 solve :: IO ()
 solve = do
--- 2025/src/Helpers/Matrix.hs
+++ 2025/src/Helpers/Matrix.hs
@@ -1,6 +1,9 @@
 module Helpers.Matrix (rowEchelonForm, augmentColumns, unaugmentColumn, showMatrix) where
-import Data.List (sortOn, intercalate, findIndex, uncons, unsnoc)
+import Data.List (sortOn, intercalate, findIndex, uncons, unsnoc, transpose)
 import Data.Maybe (fromJust)
+import Helpers.List (head')
+import Debug.Trace (trace)
+import Text.Printf (printf)
 
 -- A matrix is defined as a list of columns
 
@@ -38,15 +41,66 @@ positivePivot r = case idx of
                     Just i -> map (signum (r !! i)*) r
   where idx = findIndex (/= 0) r
 
+rowZero :: Integral a => [a] -> Bool
+rowZero = all (==0)
+
 gauss :: Integral a => [[a]] -> [[a]] -> [[a]]
 gauss m [] = m -- We're done when we're done
 gauss m (r:rest) = gauss mr rest
   where r' = positivePivot $ foldl zeroFirst r m
-        mr = m ++ [r']
+        mr = if rowZero r' then m else m ++ [r']
+
+  {-
+X X X 0 0
+0 0 X X X
+0 0 0 X X
+
+0 S 0 D 0
+
+    -}
+
+-- After gauss, do column swaps to force the staircase by propagating the free variables to the right
+-- We assume it's in triangle shape, so for any row r, the first nonzero index is zero in all following  rows
+fixColumns :: Integral a => [[a]] -> [[a]]
+fixColumns = fix 1
+  where fix colIdx m' = case uncons $ drop colIdx m' of
+                          Just (row, _) ->
+                            case findIndex (/= 0) row of
+                              Just idx -> fix (colIdx + 1) (swapColumns m' colIdx idx)
+                              Nothing -> m' -- All zeroes, we're done
+                          Nothing -> m' -- No rows left, we're done
+        swapColumns m' src dst
+          | src == dst = m'
+          | otherwise = take a t ++ [t !! b] ++ drop (a + 1) (take b t) ++ [t !! a] ++ drop (b + 1) t
+          where a = min src dst
+                b = max src dst
+                t = transpose m'
+
+gauss' :: (Show a, Integral a) => [[a]] -> [[a]]
+gauss' m
+  | null m = trace "null\n" m
+  | all (\r -> take 1 r == [0]) m = trace (printf "leading are zero: %s \n" (show m)) $ map ( 0 : ) (gauss' (map (drop 1) m))
+  | otherwise = trace (printf "row is %s\n" (show m)) $ row : map ( 0 : ) (gauss' $ sortGE (map (drop 1 . applyRow) rows))
+  where (row, rows) = fromJust $ uncons m
+        applyRow r
+          -- if [], zipWith has no effect anyyay
+          | take 1 r == [0] = r
+          | otherwise = positivePivot $ zipWith (-) r' row'
+          where row' = map (* head' r) row
+                r' = map (* head' row) r
+
+sortGE :: Integral a => [[a]] -> [[a]]
+sortGE = sortOn (\r -> (length $ takeWhile (==0) r, length (filter (/= 0) r)))
+
+rowEchelonForm :: (Show a, Integral a) => [[a]] -> [[a]]
+rowEchelonForm m = f sorted
+  where f = trace (printf "Sorted:\n%s\n\nGEd matrix:\n%s\n\n" (showMatrix sorted) (showMatrix (gauss' sorted))) $ filter (not . rowZero) . fixColumns . gauss'
+
+        sorted = sortGE m
 
 -- Use Gaussian Elimination to turn our matrix into REF
-rowEchelonForm :: Integral a => [[a]] -> [[a]]
-rowEchelonForm m = filter (not . allZero) $ gauss [h] t
+rowEchelonForm' :: Integral a => [[a]] -> [[a]]
+rowEchelonForm' m = filter (not . allZero) $ gauss [h] t
 -- Sort by leading zeroes. Swapping is allowed for Gaussian Elimination
   where sorted = sortOn (\r -> (length $ takeWhile (==0) r, length (filter (/= 0) r))) m
         (h, t) = fromJust $ uncons sorted
